b9789f76952da4fbc6b345ec239c9627c89559f5
Alan Hawse <arh@Alans-MacBook-Pro-565184.local>
2018-11-01 12:29:04 -0400

    arh projects

---
+++ arh/remote_mainapp/GoBleThread.h
@@ -0,0 +1,3 @@
+#pragma once
+
+extern void GoBleThread_start(void);
---
+++ arh/remote_resources/client_cer.c
@@ -0,0 +1,5 @@
+#include "wiced_resource.h"
+
+
+const resource_hnd_t resources_client_cer = { RESOURCE_IN_FILESYSTEM, 1224, { .fs = { 0, "client.cer" }}};
+
