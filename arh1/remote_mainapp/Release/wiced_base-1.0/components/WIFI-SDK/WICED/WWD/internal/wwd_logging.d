31859dcc32b808733901e1a615dab8a4e29cbaf4
Alan Hawse <arh@Alans-MacBook-Pro-565184.local>
2018-11-13 06:24:31 +0100

    removed trash directory

--- arh1/remote_mainapp/Release/wiced_base-1.0/components/WIFI-SDK/WICED/WWD/internal/wwd_logging.d
+++
@@ -1,5 +0,0 @@
-wiced_base-1.0/components/WIFI-SDK/WICED/WWD/internal/wwd_logging.o: \
- /Applications/ModusToolbox_1.0/libraries/wiced_base-1.0/components/WIFI-SDK/WICED/WWD/internal/wwd_logging.c \
- /Applications/ModusToolbox_1.0/libraries/wiced_base-1.0/components/WIFI-SDK/WICED/WWD/internal/wwd_logging.h
-
-/Applications/ModusToolbox_1.0/libraries/wiced_base-1.0/components/WIFI-SDK/WICED/WWD/internal/wwd_logging.h:
