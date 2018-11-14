e38f62b0691d1c3a1a10dbb161e68dcfd2345ca1
Alan Hawse <arh@Alans-MacBook-Pro-565184.local>
2018-11-14 11:31:54 +0100

    a bit of tweaking

--- firmware/e18MainController_resources/aws_client_cer.c
+++ firmware/e18MainController_resources/aws_client_cer.c
@@ -1,24 +1,24 @@
 #include "wiced_resource.h"
 
-const char resources_aws_client_cer_data[1245] = "-----BEGIN CERTIFICATE-----\r\n" \
-"MIIDWjCCAkKgAwIBAgIVAMRltUwHRNF8Rh3Gbzz4W+5Kz3MFMA0GCSqGSIb3DQEB\r\n" \
-"CwUAME0xSzBJBgNVBAsMQkFtYXpvbiBXZWIgU2VydmljZXMgTz1BbWF6b24uY29t\r\n" \
-"IEluYy4gTD1TZWF0dGxlIFNUPVdhc2hpbmd0b24gQz1VUzAeFw0xODAzMjExMzI4\r\n" \
-"MzNaFw00OTEyMzEyMzU5NTlaMB4xHDAaBgNVBAMME0FXUyBJb1QgQ2VydGlmaWNh\r\n" \
-"dGUwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQCzjWylCuN8YyEqn/dc\r\n" \
-"IM8JAd2oiepbrf2u69CfBXRCHDLVPC2DQBxQmF4mr/gDgm2Qf4FkPo6f7x1TinCb\r\n" \
-"AlS9zpeu5B/hhFxBlTf59QQarAyMA81tGZw2I8PHNJbB5QFsuE82ACVUArGFtkIj\r\n" \
-"OL/nrtbF6WnDc83fcfqT3ayw0xOYVl6YP2iqLxKf3OVv5s6NpM5EIOojEcGuBsrR\r\n" \
-"A8crwN9d1x8mDXNX23tP7oUYrA/38AT6Plp0OmoVjR/4lcVphiuyLep1jDUrBtII\r\n" \
-"93pgPy4y+PSPsy3Vd86Nk3aFl6QkbOXMGDShLOTG2EbjsS2mhdg2EwuQgeOfZGO3\r\n" \
-"dvuvAgMBAAGjYDBeMB8GA1UdIwQYMBaAFF436b+DcCtzHo83jcFBbMG5OT9KMB0G\r\n" \
-"A1UdDgQWBBT5ngDj8E70oW/Offfi/pEmhkYd+zAMBgNVHRMBAf8EAjAAMA4GA1Ud\r\n" \
-"DwEB/wQEAwIHgDANBgkqhkiG9w0BAQsFAAOCAQEAENNuNgrdcwl7LBuMirUjC0k0\r\n" \
-"DBKpyS5nyh5DaewkykoRjInPxe58RI3NkwVXL8svk+OldCxb/SlQL0KhslMv7r+z\r\n" \
-"84xCvSLwO9rO0Mrf8gA2FILU5LVRMlhBLHtBpEOeUpO9nWmiYC9qjXsG261aWS6r\r\n" \
-"gLdypR6UAgE0vM8Rrt0+41NgVJY7FZeVcqsv4wjdSaRaaqDX7J4LuC+JZVShDOXe\r\n" \
-"De4O6ISWRIbxJE3xqszXCPzJMXViqwws1JqlV6Hggt2COBl/WI5AooMIlWrrbGkA\r\n" \
-"dEjSIvQ1lrT63BpkmX6HoFg84vT6WYJjJcrntIkkjEYyTSaEA2N4Lrkd/aQofQ==\r\n" \
-"-----END CERTIFICATE-----\r\n" \
+const char resources_aws_client_cer_data[1225] = "-----BEGIN CERTIFICATE-----\n" \
+"MIIDWjCCAkKgAwIBAgIVAMRltUwHRNF8Rh3Gbzz4W+5Kz3MFMA0GCSqGSIb3DQEB\n" \
+"CwUAME0xSzBJBgNVBAsMQkFtYXpvbiBXZWIgU2VydmljZXMgTz1BbWF6b24uY29t\n" \
+"IEluYy4gTD1TZWF0dGxlIFNUPVdhc2hpbmd0b24gQz1VUzAeFw0xODAzMjExMzI4\n" \
+"MzNaFw00OTEyMzEyMzU5NTlaMB4xHDAaBgNVBAMME0FXUyBJb1QgQ2VydGlmaWNh\n" \
+"dGUwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQCzjWylCuN8YyEqn/dc\n" \
+"IM8JAd2oiepbrf2u69CfBXRCHDLVPC2DQBxQmF4mr/gDgm2Qf4FkPo6f7x1TinCb\n" \
+"AlS9zpeu5B/hhFxBlTf59QQarAyMA81tGZw2I8PHNJbB5QFsuE82ACVUArGFtkIj\n" \
+"OL/nrtbF6WnDc83fcfqT3ayw0xOYVl6YP2iqLxKf3OVv5s6NpM5EIOojEcGuBsrR\n" \
+"A8crwN9d1x8mDXNX23tP7oUYrA/38AT6Plp0OmoVjR/4lcVphiuyLep1jDUrBtII\n" \
+"93pgPy4y+PSPsy3Vd86Nk3aFl6QkbOXMGDShLOTG2EbjsS2mhdg2EwuQgeOfZGO3\n" \
+"dvuvAgMBAAGjYDBeMB8GA1UdIwQYMBaAFF436b+DcCtzHo83jcFBbMG5OT9KMB0G\n" \
+"A1UdDgQWBBT5ngDj8E70oW/Offfi/pEmhkYd+zAMBgNVHRMBAf8EAjAAMA4GA1Ud\n" \
+"DwEB/wQEAwIHgDANBgkqhkiG9w0BAQsFAAOCAQEAENNuNgrdcwl7LBuMirUjC0k0\n" \
+"DBKpyS5nyh5DaewkykoRjInPxe58RI3NkwVXL8svk+OldCxb/SlQL0KhslMv7r+z\n" \
+"84xCvSLwO9rO0Mrf8gA2FILU5LVRMlhBLHtBpEOeUpO9nWmiYC9qjXsG261aWS6r\n" \
+"gLdypR6UAgE0vM8Rrt0+41NgVJY7FZeVcqsv4wjdSaRaaqDX7J4LuC+JZVShDOXe\n" \
+"De4O6ISWRIbxJE3xqszXCPzJMXViqwws1JqlV6Hggt2COBl/WI5AooMIlWrrbGkA\n" \
+"dEjSIvQ1lrT63BpkmX6HoFg84vT6WYJjJcrntIkkjEYyTSaEA2N4Lrkd/aQofQ==\n" \
+"-----END CERTIFICATE-----\n" \
 "";
-const resource_hnd_t resources_aws_client_cer = { RESOURCE_IN_MEMORY, 1244, { .mem = { resources_aws_client_cer_data }}};
+const resource_hnd_t resources_aws_client_cer = { RESOURCE_IN_MEMORY, 1224, { .mem = { resources_aws_client_cer_data }}};
